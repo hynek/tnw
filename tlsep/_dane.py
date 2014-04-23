9aae0aa90449ee181fe7fcf4e2ae0d766676819c
Hynek Schlawack <schlawack@variomedia.de>
2014-04-23 21:49:15 +0200

    Add TLSARecord

--- tlsep/_dane.py
+++ tlsep/_dane.py
@@ -1,13 +1,17 @@
 # -*- test-case-name: tlsep.test.test_dane -*-
 # Copyright (c) Richard Wall, Hynek Schwlack
 # See LICENSE for details.
-import binascii
+
+import hashlib
 import getdns
 
 
 from OpenSSL import crypto
 
+from twisted.python.constants import ValueConstant, Values
 from twisted.python.util import FancyStrMixin
+
+from tlsep._x509 import extractPublicKey
 
 
 def tlsaDomainName(parentDomain, port, proto):
@@ -39,30 +43,66 @@ class LookupError(FancyStrMixin, Exception):
                     return k
 
 
+class USAGE(Values):
+    PKIX_TA = ValueConstant(0)
+    PKIX_EE = ValueConstant(1)
+    DANE_TA = ValueConstant(2)
+    DANE_EE = ValueConstant(3)
 
-class TLSA_Cert(FancyStrMixin, object):
-    type = 0
 
-    def __init__(self, cert):
+class SELECTOR(Values):
+    CERT = ValueConstant(0)
+    SPKI = ValueConstant(1)
+
+
+class MATCHING_TYPE(Values):
+    FULL = ValueConstant(0)
+    SHA_256 = ValueConstant(1)
+    SHA_512 = ValueConstant(2)
+
+
+SELECTOR_MAP = {
+    SELECTOR.CERT: lambda other: crypto.dump_certificate(
+        crypto.FILETYPE_ASN1,
+        other
+    ),
+    SELECTOR.SPKI: extractPublicKey,
+}
+
+MATCHING_TYPE_MAP = {
+    MATCHING_TYPE.FULL: lambda other: other,
+    MATCHING_TYPE.SHA_256: lambda other: hashlib.sha256(other).digest(),
+    MATCHING_TYPE.SHA_512: lambda other: hashlib.sha512(other).digest(),
+}
+
+
+class TLSARecord(FancyStrMixin, object):
+    showAttributes = ('usage', 'selector', 'matchingType')
+
+    def __init__(self, payload, usage, selector, matchingType):
+        self.payload = bytes(payload)
+
+        self.usage = USAGE.lookupByValue(usage)
+        self.selector = SELECTOR.lookupByValue(selector)
+        self.matchingType = MATCHING_TYPE.lookupByValue(matchingType)
+
+        self._select = SELECTOR_MAP.get(self.selector)
+        if self._select is None:
+            raise ValueError("Invalid selector: {}".format(selector))
+
+        self._transform = MATCHING_TYPE_MAP.get(self.matchingType)
+        if self._transform is None:
+            raise ValueError("Invalid matching type: {}".format(matchingType))
+
+
+    def matchesCertificate(self, cert):
         """
+        @type cert: x509
+
+        @rtype: bool
         """
-        self.cert = crypto.load_certificate(crypto.FILETYPE_ASN1, cert[:])
+        return self.payload == self._transform(self._select(cert))
 
-
-class TLSA_SPKI(FancyStrMixin, object):
-    type = 1
-
-    showAttributes = ('spki',)
-
-    def __init__(self, spki):
-        """
-        """
-        self.spki = binascii.hexlify(spki)
-
-
-TLSA_SELECTOR_MAP = {}
-for t in (TLSA_Cert, TLSA_SPKI):
-    TLSA_SELECTOR_MAP[t.type] = t
 
 
 def tlsa(parentDomain, port, proto, getdns=getdns):
@@ -83,7 +123,7 @@ def tlsa(parentDomain, port, proto, getdns=getdns):
     ctx = getdns.context_create()
     extensions = {
         "return_both_v4_and_v6": getdns.GETDNS_EXTENSION_TRUE,
-        "dnssec_return_only_secure": getdns.GETDNS_EXTENSION_TRUE
+        "dnssec_return_only_secure": getdns.GETDNS_EXTENSION_FALSE,
     }
     results = getdns.general(ctx,
                              request_type=getdns.GETDNS_RRTYPE_TLSA,
@@ -91,13 +131,12 @@ def tlsa(parentDomain, port, proto, getdns=getdns):
                              extensions=extensions)
 
     if results["status"] == getdns.GETDNS_RESPSTATUS_GOOD:
-        tlsaType = (
-            TLSA_SELECTOR_MAP[results['replies_tree']
-                              [0]['answer'][0]['rdata']['selector']]
-        )
-        return tlsaType(
-            results['replies_tree']
-            [0]['answer'][0]['rdata']['certificate_association_data']
+        rdata = results['replies_tree'][0]['answer'][0]['rdata']
+        return TLSARecord(
+            rdata['certificate_association_data'],
+            3,
+            rdata["selector"],
+            rdata["matching_type"],
         )
 
     raise LookupError(results['status'])
