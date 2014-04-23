7fe5a0c03e93932603d20d8d44152dc3bb4816f4
rwall <richard@the-moon.net>
2014-04-23 17:29:25 +0200

    docstrings

--- tlsep/dane.py
+++ tlsep/dane.py
@@ -5,20 +5,47 @@ import binascii
 import getdns
 
 
-def tlsaDomainName(parent_domain, port, proto):
+def tlsaDomainName(parentDomain, port, proto):
     """
     Return a TLSA domain name.
-    """
-    return "_%s._%s.%s" % (port, proto, parent_domain)
 
+    @param parentDomain: The domain with which the TLSA record is associated.
+    @param port: The port number with which the TLSA record is associated.
+    @param proto: The IP protocol with which the TLSA record is associated.
+    """
+    return "_%s._%s.%s" % (port, proto, parentDomain)
 
 
 class LookupError(Exception):
+    """
+    Raised for any getdns return status that isn't GOOD.
+    """
     pass
 
 
+class TLSAFullCertificate(object):
+    pass
 
-def tlsa(parent_domain, port, proto, getdns=getdns):
+
+class TLSASubjectPublicInfo(object):
+    pass
+
+
+def tlsa(parentDomain, port, proto, getdns=getdns):
+    """
+    Lookup a TLSA record and return a TLSA type depending on the TLSA
+    selector type in the record.
+
+    @see: U{http://tools.ietf.org/html/draft-ietf-dane-registry-acronyms-01#section-2.2}
+
+    @param parentDomain: The domain with which the TLSA record is associated.
+    @param port: The port number with which the TLSA record is associated.
+    @param proto: The IP protocol with which the TLSA record is associated.
+    @param getdns: An optional getdnsapi object. For testing purposes.
+
+    @returns: A TLSA record instance corresponding to the selector type of the
+        record.
+    """
     ctx = getdns.context_create()
     extensions = {
         "return_both_v4_and_v6" : getdns.GETDNS_EXTENSION_TRUE,
@@ -26,7 +53,7 @@ def tlsa(parent_domain, port, proto, getdns=getdns):
     }
     results = getdns.general(ctx,
                              request_type=getdns.GETDNS_RRTYPE_TLSA,
-                             name=tlsaDomainName(parent_domain, port, proto),
+                             name=tlsaDomainName(parentDomain, port, proto),
                              extensions=extensions)
 
     if results["status"] == getdns.GETDNS_RESPSTATUS_GOOD:
