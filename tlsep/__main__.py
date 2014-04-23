23da4dda48a5e27cb9fd8536722bcf66b55987eb
rwall <richard@the-moon.net>
2014-04-23 16:12:56 +0200

    Use the tlsDomainName function

--- tlsep/__main__.py
+++ tlsep/__main__.py
@@ -2,6 +2,10 @@
 # Copyright (c) Richard Wall, Hynek Schwlack
 # See LICENSE for details.
 
+"""
+eg tlsep full.cert.getdnsapi.net 443 tcp
+"""
+
 import sys
 
 from twisted.internet import task, threads
@@ -10,11 +14,11 @@ from tlsep import dane
 
 
 def main():
-    if len(sys.argv) != 2:
-        print "Usage: {0} hostname".format(sys.argv[0])
+    if len(sys.argv) != 4:
+        print "Usage: {0} parent_domain port protocol".format(sys.argv[0])
         sys.exit(1)
 
     def _main(reactor):
-        return threads.deferToThread(dane.dnssec, sys.argv[1])
+        return threads.deferToThread(dane.tlsa, *sys.argv[1:])
 
     task.react(_main)
--- tlsep/dane.py
+++ tlsep/dane.py
@@ -13,13 +13,17 @@ def tlsaDomainName(parent_domain, port, proto):
 
 
 
-def tlsa(hostname, port, proto):
+def tlsa(parent_domain, port, proto):
     ctx = getdns.context_create()
     extensions = {
         "return_both_v4_and_v6" : getdns.GETDNS_EXTENSION_TRUE,
         "dnssec_return_only_secure" : getdns.GETDNS_EXTENSION_TRUE
     }
-    results = getdns.address(ctx, name=hostname, extensions=extensions)
+    results = getdns.general(ctx,
+                             request_type=getdns.GETDNS_RRTYPE_TLSA,
+                             name=tlsaDomainName(parent_domain, port, proto),
+                             extensions=extensions)
+
     if results["status"] == getdns.GETDNS_RESPSTATUS_GOOD:
         sys.stdout.write("Addresses: ")
 
@@ -28,42 +32,5 @@ def tlsa(hostname, port, proto):
         sys.stdout.write("\n\n")
         print "Entire results tree: "
         pprint.pprint(results)
-    if results["status"] == getdns.GETDNS_RESPSTATUS_NO_NAME:
-        print "{0} not found".format(sys.argv[1])
-
-
-
-dnssec_status = {
-    "GETDNS_DNSSEC_SECURE" : 400,
-    "GETDNS_DNSSEC_BOGUS" : 401,
-    "GETDNS_DNSSEC_INDETERINATE" : 402,
-    "GETDNS_DNSSEC_INSECURE" : 403,
-    "GETDNS_DNSSEC_NOT_PERFORMED" : 404
-}
-
-
-def dnssec_message(value):
-    for message in dnssec_status.keys():
-        if dnssec_status[message] == value:
-            return message
-
-
-
-def dnssec(hostname):
-    ctx = getdns.context_create()
-    extensions = { "return_both_v4_and_v6" : getdns.GETDNS_EXTENSION_TRUE,
-                   "dnssec_return_only_secure" : getdns.GETDNS_EXTENSION_TRUE }
-    results = getdns.address(ctx, name=hostname, extensions=extensions)
-    if results["status"] == getdns.GETDNS_RESPSTATUS_GOOD:
-        sys.stdout.write("Addresses: ")
-        for addr in results["just_address_answers"]:
-            print " {0}".format(addr["IPSTRING"])
-        sys.stdout.write("\n")
-
-        for result in results["replies_tree"]:
-            if "dnssec_status" in result.keys():
-                print "{0}: dnssec_status: {1}".format(result["canonical_name"],
-                                                       dnssec_message(result["dnssec_status"]))
-
     if results["status"] == getdns.GETDNS_RESPSTATUS_NO_NAME:
         print "{0} not found".format(sys.argv[1])
