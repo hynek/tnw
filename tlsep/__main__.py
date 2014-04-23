3f065d3e6b54912b2d78a70b5fda98d0476c3f09
rwall <richard@the-moon.net>
2014-04-24 00:34:15 +0200

    Check the server cert using matchesCertificate

--- tlsep/__main__.py
+++ tlsep/__main__.py
@@ -8,16 +8,28 @@ eg tlsep full.cert.getdnsapi.net 443 tcp
 
 import sys
 
-from twisted.internet import task, threads
+from twisted.internet import task, threads, defer
 
-from tlsep import _dane
+from tlsep import _dane, _tls
+
+
+def printResult(res):
+    tlsaRecord, serverCertificate = res
+    print tlsaRecord.matchesCertificate(serverCertificate)
+    print tlsaRecord
+
+def _main(reactor, parent_domain, port, proto):
+    d = defer.gatherResults([
+        threads.deferToThread(_dane.lookup_tlsa_records, parent_domain, port, proto),
+        _tls.retrieveCertificate(parent_domain, port)
+    ])
+    d.addCallback(printResult)
+    return d
+
 
 def main():
     if len(sys.argv) != 4:
         print "Usage: {0} parent_domain port protocol".format(sys.argv[0])
         sys.exit(1)
 
-    def _main(reactor):
-        return threads.deferToThread(_dane.lookup_tlsa_records, *sys.argv[1:])
-
-    task.react(_main)
+    task.react(_main, sys.argv[1:])
